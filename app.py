2c0d47745e556f6b7b06d5a5c29e114bd1e7c98d
Shrvan Singh Thakur <thakurshrvan2004@gmail.com>
2025-05-06 10:27:48 +0530

    app.py update3

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def ask_bot():
         return jsonify({"response": f"Error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int (os.environ.get("PORT",10000)) 
+    app.run(host='0.0.0.0',port=port,debug=True)
